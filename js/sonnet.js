bfa6b8cbeb4983b2887765f39d5a576ec82cb336
Ben Marks <onodric@yahoo.com>
2016-10-17 14:02:49 -0500

    re-engineered for goodness

--- js/sonnet.js
+++ js/sonnet.js
@@ -1,11 +1,14 @@
 
 var sonnet = document.getElementById("sonnet");
+var preNode = document.createElement("pre");
+
 // 2. Take the contents of the sonnet div and place it in a variable
 var sonnetText = sonnet.innerHTML;
 
+sonnetText = sonnetText.replace(/\<pre\>|\<\/pre\>/g, "");
 
 // 3. Find and output the starting position of the word "orphans"
-console.log('"orphans" are found at: ', sonnetText.indexOf("orphans"));
+console.log('"orphans" are found at position: ', sonnetText.indexOf("orphans"));
 
 // 4. Output the number of characters in the sonnet
 console.log('Character Count: ', (sonnetText.length + 1));
@@ -14,7 +17,11 @@ console.log('Character Count: ', (sonnetText.length + 1));
 var newSonnet = sonnetText.replace('winter', 'yuletide');
 
 // 6. Replace all occurances of the string "the" with "a large"
-var newerSonnet = newSonnet.replace(/ the /gi, ' a large ');
+var newerSonnet = newSonnet.replace(/ the |The /gi, ' a large ');
+
+var sonnetNode = document.createTextNode(newerSonnet);
+
+preNode.appendChild(sonnetNode);
 
 // 7. Set the content of the sonnet div with the new string
-sonnet.innerHTML = newerSonnet;
+sonnet.replaceChild(preNode, sonnet.childNodes[1]);
